3dfd812860b72149867de24ab044de89389d3cba
Azmeer K <azmeer.khawaja@outlook.com>
2023-10-29 21:39:25 +0100

    Adjusted file path.

--- src/main/se/kth/id1212/simulator/ExceptionLogger.java
+++ src/main/se/kth/id1212/simulator/ExceptionLogger.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 
 public class ExceptionLogger {
     public static void logExceptionToFile(Exception exception) {
-        String filePath = "Task_2\\src\\main\\se\\kth\\id1212\\simulator\\exception_log.txt";
+        String filePath = "Task_2/src/main/se/kth/id1212/simulator/log.txt";
         try {
             PrintWriter writer = new PrintWriter(new FileWriter(filePath, true));
             exception.printStackTrace(writer);
