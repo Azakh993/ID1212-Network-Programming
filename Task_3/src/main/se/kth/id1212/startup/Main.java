49232dc278762bf40d7d0c9c27af90833b5ce72a
Azmeer K <azmeer.khawaja@outlook.com>
2023-10-29 21:40:17 +0100

    Added code to run EmailSender.

--- Task_3/src/main/se/kth/id1212/startup/Main.java
+++ Task_3/src/main/se/kth/id1212/startup/Main.java
@@ -1,10 +1,31 @@
 package main.se.kth.id1212.startup;
 
 import main.se.kth.id1212.imap_retrieve.EmailRetriever;
+import main.se.kth.id1212.smtp_send.EmailSender;
+
+import java.time.LocalTime;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         EmailRetriever emailRetriever = new EmailRetriever();
         emailRetriever.get_latest_email();
+        emailRetriever.logout();
+
+        System.out.println("Enter recipient email address: ");
+        Scanner scanner = new Scanner(System.in);
+        String recipient = scanner.nextLine();
+
+        String message = "Date: " + LocalTime.now() + "\r\n" +
+                "From: SMTP Test Program <aaak@kth.se>\r\n" +
+                "To: Test Subject <" + recipient + ">\r\n" +
+                "Subject: Test Subject Line\r\n" +
+                "\r\n" +
+                "Test Body";
+
+        EmailSender emailSender = new EmailSender();
+
+        emailSender.send_email(recipient, message);
+        emailSender.logout();
     }
 }
