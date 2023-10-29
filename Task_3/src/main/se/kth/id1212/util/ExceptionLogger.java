11568092a28e41f2419e3574647a1b8ce6c685d1
Azmeer K <azmeer.khawaja@outlook.com>
2023-10-29 16:49:27 +0100

    Added utility class for logging exceptions.

---
+++ Task_3/src/main/se/kth/id1212/util/ExceptionLogger.java
@@ -0,0 +1,18 @@
+package main.se.kth.id1212.util;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class ExceptionLogger {
+    public static void logExceptionToFile(Exception exception) {
+        String filePath = "Task_3\\src\\main\\se\\kth\\id1212\\exception_log.txt";
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(filePath, true));
+            exception.printStackTrace(writer);
+            writer.println();
+            writer.flush();
+        } catch (Exception ioException) {
+            ioException.printStackTrace();
+        }
+    }
+}
