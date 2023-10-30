a9d5e7e4397b60c979e6c491fb5ff7c09cfea8d0
Azmeer K <azmeer.khawaja@outlook.com>
2023-10-30 19:37:24 +0100

    Added documentation. Added code for better handling of client/server disconnection.

--- src/main/se/kth/id1212/server/ClientHandler.java
+++ src/main/se/kth/id1212/server/ClientHandler.java
@@ -6,18 +6,32 @@ import java.util.ArrayList;
 
 import static main.se.kth.id1212.server.ChatServer.logExceptionToFile;
 
+/**
+ * Handles the communication between the server and a client.
+ */
 class ClientHandler implements Runnable {
     private final Socket client_socket;
     private final String client_name;
     private final ArrayList<ClientHandler> all_connected_clients;
     private ObjectOutputStream outgoing_message_stream;
 
+    /**
+     * Creates a new instance of a client handler.
+     * @param client_socket          The socket of the client.
+     * @param connected_clients      A list of all connected clients.
+     */
     ClientHandler(Socket client_socket, ArrayList<ClientHandler> connected_clients) {
         this.client_socket = client_socket;
         client_name = client_socket.getInetAddress().getHostName();
         all_connected_clients = connected_clients;
     }
 
+    /**
+     * Starts the communication between the server and the client. The server waits for incoming messages from the
+     * client via the input stream. When a message is received, it is broadcasted to all connected clients via the
+     * output stream. If the client sends the message "!exit", its connection is closed and the exit is broadcasted.
+     * If the client disconnects without sending "!exit", the server will broadcast this to all connected clients.
+     */
     @Override
     public void run() {
         try {
@@ -43,12 +57,21 @@ class ClientHandler implements Runnable {
                 }
             }
 
+        } catch (EOFException exception) {
+            broadcast("has left the chat.");
+            System.out.println(client_name + " disconnected.");
+            all_connected_clients.remove(this);
         } catch (Exception exception) {
             logExceptionToFile(exception);
         }
 
     }
 
+    /**
+     * Broadcasts a message to all connected clients except the current client.
+     * The message is sent via the output stream of each client in the all_connected_clients ArrayList.
+     * @param message   The message to be broadcasted.
+     */
     private void broadcast(String message) {
         for (ClientHandler client : all_connected_clients) {
             if (!this.equals(client) && client.outgoing_message_stream != null) {
@@ -62,6 +85,9 @@ class ClientHandler implements Runnable {
         }
     }
 
+    /**
+     * Closes the connection to the client.
+     */
     private void close_connection() {
         try {
             all_connected_clients.remove(this);
