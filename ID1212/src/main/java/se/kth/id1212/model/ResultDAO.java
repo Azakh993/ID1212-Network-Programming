21521e14e008e1160ec209ef8c95cb6c2f8297bf
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-05 20:34:26 +0100

    Added methods that are likely to be used for required functionality.

---
+++ ID1212/src/main/java/se/kth/id1212/model/ResultDAO.java
@@ -0,0 +1,9 @@
+package se.kth.id1212.model;
+
+import java.util.HashMap;
+
+public interface ResultDAO<Result> {
+    HashMap<Integer, Result> getAllResults(String username);
+    void updateResult(Integer userId, Integer quizId);
+    void addResult(Integer userId, Integer quizId);
+}
