a58b5006e1e2cc5a576aa9e99e01fc1ad78c3a68
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-05 21:12:18 +0100

    Removed Optional.

--- ID1212/src/main/java/se/kth/id1212/model/UserDAO.java
+++ ID1212/src/main/java/se/kth/id1212/model/UserDAO.java
@@ -1,7 +1,5 @@
 package se.kth.id1212.model;
 
-import java.util.Optional;
-
 public interface UserDAO {
-    Optional<User> getUser(String username, String password);
+    User getUser(String username, String password);
 }
