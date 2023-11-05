30287596e01543c148db0f67605505792102ed24
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-05 10:22:05 +0100

    Added temporary implementation for testing of login.jsp and LoginServlet.

--- ID1212/src/main/java/se/kth/id1212/integration/UserDAOImpl.java
+++ ID1212/src/main/java/se/kth/id1212/integration/UserDAOImpl.java
@@ -1,7 +1,13 @@
 package se.kth.id1212.integration;
 
+import se.kth.id1212.model.User;
 import se.kth.id1212.model.UserDAO;
 
-public class UserDAOImpl implements UserDAO {
+import java.util.Optional;
 
+public class UserDAOImpl implements UserDAO {
+    @Override
+    public Optional<User> getUser(String username, String password) {
+        return Optional.empty();
+    }
 }
