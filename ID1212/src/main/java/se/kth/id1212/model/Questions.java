bb7e55358e66ce87c8ea7f96eb4925a3190f7ce7
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-06 18:35:22 +0100

    Renamed to more accurately represent entity.

--- ID1212/src/main/java/se/kth/id1212/model/Questions.java
+++
@@ -1,4 +0,0 @@
-package se.kth.id1212.model;
-
-public record Questions(Integer id, String question, String answer, String[] options) {
-}
--- ID1212/src/main/java/se/kth/id1212/model/QuestionsDAO.java
+++
@@ -1,5 +0,0 @@
-package se.kth.id1212.model;
-
-public interface QuestionsDAO {
-    Questions[] getAllQuestions(Integer quizID);
-}
