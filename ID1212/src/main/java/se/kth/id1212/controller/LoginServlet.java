43dd41dee1c7847b6137fe395e8290b51042e1cf
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-06 18:33:20 +0100

    Refactored; simplified and using utility class methods.

--- ID1212/src/main/java/se/kth/id1212/controller/LoginServlet.java
+++ ID1212/src/main/java/se/kth/id1212/controller/LoginServlet.java
@@ -22,46 +22,37 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            request.getRequestDispatcher("/login.jsp").forward(request, response);
-        } catch (Exception exception) {
-            ExceptionLogger.log(exception);
-        }
+        ControllerUtil.forward_request(request, response, "/login.jsp");
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-        HttpSession session = request.getSession();
+        login(request);
+        String userID = ControllerUtil.validate_login_state(request, response);
 
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-
-        boolean logged_in = false;
-
-        if (username != null || password != null) {
-            logged_in = login(session, username, password);
-        }
-
-        try {
-            if (logged_in) {
-                response.sendRedirect(request.getContextPath() + "/dashboard");
-            } else {
+        if (userID != null) {
+            ControllerUtil.redirect_request(request, response, "/dashboard");
+        } else {
+            try {
                 response.sendError(401, "Unauthorized");
+            } catch (Exception exception) {
+                ExceptionLogger.log(exception);
             }
-        } catch (Exception exception) {
-            ExceptionLogger.log(exception);
         }
     }
 
-    private boolean login(HttpSession session, String username, String password) {
-        User user = this.userDAO.getUser(username, password);
+    private void login(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
 
-        if (user == null) {
-            return false;
+        if(username != null || password != null) {
+            User user = this.userDAO.getUser(username, password);
+
+            if (user != null) {
+                String userID = user.id().toString();
+                HttpSession session = request.getSession();
+                session.setAttribute("USERID", userID);
+            }
         }
-
-        String userID = user.id().toString();
-        session.setAttribute("USERID", userID);
-        return true;
     }
 }
