41329f10c6862940f775789c4b148b8175530d76
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-06 18:37:55 +0100

    Added code to handle a request for viewing a quiz. Manual testing indicates correct functionality.

--- ID1212/src/main/java/se/kth/id1212/controller/QuizServlet.java
+++ ID1212/src/main/java/se/kth/id1212/controller/QuizServlet.java
@@ -1,4 +1,52 @@
 package se.kth.id1212.controller;
 
-public class QuizServlet {
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import se.kth.id1212.integration.QuestionDAOImpl;
+import se.kth.id1212.integration.QuizDAOImpl;
+import se.kth.id1212.model.*;
+
+@WebServlet(name = "QuizServlet", urlPatterns = {"/quiz"})
+public class QuizServlet extends HttpServlet {
+    private QuizDAO<Quiz> quizDAO;
+    private QuestionDAO<Question> questionDAO;
+
+    @Override
+    public void init(ServletConfig config) {
+        this.quizDAO = new QuizDAOImpl();
+        this.questionDAO = new QuestionDAOImpl();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        String userID = ControllerUtil.validate_login_state(request, response);
+
+        if (userID != null) {
+            setQuizSubject(request);
+            setQuizQuestions(request);
+            ControllerUtil.forward_request(request, response, "/quiz.jsp");
+        }
+    }
+
+    private void setQuizSubject(HttpServletRequest request) {
+        Integer quizID = Integer.valueOf(request.getParameter("quizID"));
+        Quiz quiz = this.quizDAO.getQuiz(quizID);
+        String quizSubject = quiz.subject();
+        request.setAttribute("quizSubject", quizSubject);
+    }
+
+    private void setQuizQuestions(HttpServletRequest request) {
+        Integer quizID = Integer.valueOf(request.getParameter("quizID"));
+        Question[] questions = this.questionDAO.getAllQuestions(quizID);
+        request.setAttribute("questionsAndOptions", questions);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
 }
+
--- ID1212/src/main/webapp/quiz.jsp
+++ ID1212/src/main/webapp/quiz.jsp
@@ -5,12 +5,44 @@
   Time: 21:48
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page import="se.kth.id1212.model.Question" %>
+
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Title</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/CSS/quiz.css">
+    <title>Quiz Page</title>
 </head>
 <body>
-
+<h1>Quiz: <%= request.getAttribute("quizSubject") %></h1>
+<form action="" method="post">
+    <%
+        Question[] questions = (Question[]) request.getAttribute("questionsAndOptions");
+        for (int i=0; i < questions.length; i++) {
+    %>
+    <div class="question">
+        <p><strong>Question <%= i+1 %>:</strong> <%= questions[i].questionText() %></p>
+        <ul class="answers">
+            <%
+                for (String option : questions[i].options()) {
+            %>
+            <li>
+                <label>
+                    <input type="radio" name="question<%= questions[i].id() %>" value="<%= option %>">
+                    <%= option %>
+                </label>
+            </li>
+            <%
+                }
+            %>
+        </ul>
+    </div>
+    <%
+        }
+    %>
+    <input type="submit" value="Submit Quiz">
+</form>
 </body>
 </html>
