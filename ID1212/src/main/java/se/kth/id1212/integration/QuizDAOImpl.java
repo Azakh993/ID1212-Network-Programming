143562791cf8904d1fc9c03248d2481fca042f7e
Azmeer K <azmeer.khawaja@outlook.com>
2023-11-08 21:28:57 +0100

    Implemented code for getting a specific quiz as well as all quizzes from the database.

--- ID1212/src/main/java/se/kth/id1212/integration/QuizDAOImpl.java
+++ ID1212/src/main/java/se/kth/id1212/integration/QuizDAOImpl.java
@@ -3,19 +3,55 @@ package se.kth.id1212.integration;
 import se.kth.id1212.model.Quiz;
 import se.kth.id1212.model.QuizDAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizDAOImpl implements QuizDAO< Quiz > {
+    private Connection connection = DatabaseHandler.connect();
+
     @Override
     public Quiz getQuiz(Integer quizID) {
-        return new Quiz(1, "Test 1");
+        Quiz quiz = null;
+        String sql = "SELECT * FROM QUIZZES WHERE ID = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, quizID);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    int id = resultSet.getInt("ID");
+                    String subject = resultSet.getString("SUBJECT");
+                    quiz = new Quiz(id, subject);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return quiz;
     }
 
     @Override
     public Quiz[] getAllQuizzes() {
-        Quiz[] quizzes = new Quiz[4];
-        quizzes[0] = new Quiz(1, "Test 1");
-        quizzes[1] = new Quiz(2, "Test 2");
-        quizzes[2] = new Quiz(3, "Test 3");
-        quizzes[3] = new Quiz(4, "Test 4");
-        return quizzes;
+        List< Quiz > quizzes = new ArrayList<>();
+        String sql = "SELECT * FROM QUIZZES";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("ID");
+                String subject = resultSet.getString("SUBJECT");
+                Quiz quiz = new Quiz(id, subject);
+                quizzes.add(quiz);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return quizzes.toArray(new Quiz[0]);
     }
 }
