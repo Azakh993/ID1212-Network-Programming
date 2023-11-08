a0add97e577f1992d43b98485167c5f0c9a40d48
Azmeer K <azmeer.khawaja@outlook.com>
2023-11-08 21:29:30 +0100

    Implemented code for getting a specific user.

--- ID1212/src/main/java/se/kth/id1212/integration/UserDAOImpl.java
+++ ID1212/src/main/java/se/kth/id1212/integration/UserDAOImpl.java
@@ -3,9 +3,30 @@ package se.kth.id1212.integration;
 import se.kth.id1212.model.User;
 import se.kth.id1212.model.UserDAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserDAOImpl implements UserDAO {
+    private Connection connection = DatabaseHandler.connect();
+
     @Override
     public User getUser(String username, String password) {
-        return new User(1, "test", "test");
+        User user = null;
+        if (connection != null) {
+            try {
+                PreparedStatement statement = connection.prepareStatement("SELECT * FROM USERS WHERE USERNAME = ? AND PASSWORD = ?");
+                statement.setString(1, username);
+                statement.setString(2, password);
+                ResultSet resultSet = statement.executeQuery();
+                if (resultSet.next()) {
+                    user = new User(resultSet.getInt("ID"), resultSet.getString("USERNAME"), resultSet.getString("PASSWORD"));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return user;
     }
 }
