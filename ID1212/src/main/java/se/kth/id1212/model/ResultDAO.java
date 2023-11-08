fae92612932a6bc840168c2ada1ea53499c21546
Azmeer K <azmeer.khawaja@outlook.com>
2023-11-08 21:25:43 +0100

    Corrected method signature.

--- ID1212/src/main/java/se/kth/id1212/model/ResultDAO.java
+++ ID1212/src/main/java/se/kth/id1212/model/ResultDAO.java
@@ -3,7 +3,7 @@ package se.kth.id1212.model;
 import java.util.HashMap;
 
 public interface ResultDAO< Result > {
-    HashMap< Integer, Result > getAllResults(String username);
+    HashMap< Integer, Result > getAllResults(Integer userID);
 
     void addResult(Integer userId, Integer quizId, Integer points);
 }
