a8997f4c3fa35f8c20ccbc8af5fd50e6d5c9acfa
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-23 23:38:54 +0100

    Changed database to Derby. Added documentation.

--- src/main/java/se/kth/id1212/integration/DatabaseHandler.java
+++ src/main/java/se/kth/id1212/integration/DatabaseHandler.java
@@ -12,12 +12,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * Handles the connection to the database and provides functionality for database-related operations.
+ */
 public class DatabaseHandler {
+    /**
+     * The singleton instance of the database connection.
+     */
     private static Connection connection = null;
 
+    /**
+     * Establishes a connection to the database. If the database file exists, it connects to the existing database.
+     * Otherwise, it creates a new database and initializes it with the contents of a SQL script.
+     *
+     * @return The established database connection.
+     */
     public static Connection connect() {
-        Path filePath = Paths.get("src", "main", "database", "Quiz.db");
+        Path filePath = Paths.get("src", "main", "database", "db.lck");
         File databaseFile = new File(String.valueOf(filePath.toAbsolutePath()));
+
+        Path dbDirectoryPath = Paths.get("src", "main", "database");
+        File dbDirectory = new File(String.valueOf(dbDirectoryPath.toAbsolutePath()));
 
         if (connection != null) {
             return connection;
@@ -25,24 +40,29 @@ public class DatabaseHandler {
 
         if (databaseFile.exists()) {
             try {
-                Class.forName("org.sqlite.JDBC");
-                connection = DriverManager.getConnection("jdbc:sqlite:" + databaseFile);
+                Class.forName("org.apache.derby.iapi.jdbc.AutoloadedDriver");
+                connection = DriverManager.getConnection("jdbc:derby:" + dbDirectory);
             } catch (ClassNotFoundException | SQLException exception) {
                 ExceptionLogger.log(exception);
             }
         } else {
-            connection = createNewDatabase(databaseFile);
+            connection = createNewDatabase(dbDirectory);
         }
 
         return connection;
     }
 
-    private static Connection createNewDatabase(File databaseFile) {
+    /**
+     * Creates a new database and initializes it with the contents of a SQL script.
+     *
+     * @param dbDirectory The directory where the new database will be created.
+     * @return The established connection to the newly created database.
+     */
+    private static Connection createNewDatabase(File dbDirectory) {
         Connection connection = null;
         try {
-            Class.forName("org.sqlite.JDBC");
-
-            connection = DriverManager.getConnection("jdbc:sqlite:" + databaseFile.getAbsolutePath());
+            Class.forName("org.apache.derby.iapi.jdbc.AutoloadedDriver");
+            connection = DriverManager.getConnection("jdbc:derby:" + dbDirectory.getAbsolutePath() + ";create=true");
 
             Path scriptPath = Paths.get("src", "main", "resources", "PopulateDB.sql").toAbsolutePath();
 
@@ -57,6 +77,13 @@ public class DatabaseHandler {
         return connection;
     }
 
+    /**
+     * Executes a SQL script on the given database connection.
+     *
+     * @param connection The database connection on which the SQL script will be executed.
+     * @param sqlScript  The SQL script to be executed.
+     * @throws SQLException If an SQL exception occurs during script execution.
+     */
     public static void executeSqlScript(Connection connection, String sqlScript) throws SQLException {
         String[] sqlStatements = sqlScript.split(";");
         try (Statement statement = connection.createStatement()) {
