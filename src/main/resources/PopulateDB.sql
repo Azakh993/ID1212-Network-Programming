8d15bb52aa7d75569a078217a27aebf9392aeb08
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-23 23:40:03 +0100

    Modified script to work with Derby.

--- src/main/resources/PopulateDB.sql
+++ src/main/resources/PopulateDB.sql
@@ -1,61 +1,48 @@
--- Create the USERS table
 CREATE TABLE USERS
 (
-    ID       INTEGER PRIMARY KEY,
-    USERNAME TEXT NOT NULL,
-    PASSWORD TEXT NOT NULL
+    ID       INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    USERNAME VARCHAR(32) UNIQUE NOT NULL,
+    PASSWORD VARCHAR(32) NOT NULL
 );
 
--- Create the QUIZZES table
 CREATE TABLE QUIZZES
 (
-    ID      INTEGER PRIMARY KEY,
-    SUBJECT TEXT NOT NULL
+    ID      INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    SUBJECT VARCHAR(64) NOT NULL
 );
 
--- Create the QUESTIONS table
 CREATE TABLE QUESTIONS
 (
-    ID      INTEGER PRIMARY KEY,
-    TEXT    TEXT NOT NULL,
-    OPTIONS TEXT NOT NULL,
-    ANSWER  TEXT NOT NULL
+    ID      INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    TEXT    VARCHAR(64) NOT NULL,
+    OPTIONS VARCHAR(255) NOT NULL,
+    ANSWER  VARCHAR(64) NOT NULL
 );
 
--- Create the SELECTOR table to associate quizzes and questions
 CREATE TABLE SELECTOR
 (
-    QUIZ_ID     INTEGER NOT NULL,
-    QUESTION_ID INTEGER NOT NULL,
-    FOREIGN KEY (QUIZ_ID) REFERENCES QUIZZES (ID),
-    FOREIGN KEY (QUESTION_ID) REFERENCES QUESTIONS (ID)
+    QUIZ_ID     INTEGER NOT NULL REFERENCES QUIZZES(ID),
+    QUESTION_ID INTEGER NOT NULL REFERENCES QUESTIONS(ID)
 );
 
--- Create the RESULTS table
 CREATE TABLE RESULTS
 (
-    ID      INTEGER PRIMARY KEY,
-    USER_ID INTEGER NOT NULL,
-    QUIZ_ID INTEGER NOT NULL,
-    SCORE   INTEGER NOT NULL,
-    FOREIGN KEY (USER_ID) REFERENCES USERS (ID),
-    FOREIGN KEY (QUIZ_ID) REFERENCES QUIZZES (ID)
+    ID      INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    USER_ID INTEGER NOT NULL REFERENCES USERS(ID),
+    QUIZ_ID INTEGER NOT NULL REFERENCES QUIZZES(ID),
+    SCORE   INTEGER NOT NULL
 );
 
--- Populate the USERS table with demo data
 INSERT INTO USERS (USERNAME, PASSWORD)
 VALUES ('aaak@kth.se', 'password1'),
        ('ebeshir@kth.se', 'password2'),
        ('test@kth.se', 'password3');
 
--- Populate the QUIZZES table with demo data
 INSERT INTO QUIZZES (SUBJECT)
 VALUES ('Math Quiz'),
        ('Science Quiz'),
        ('History Quiz');
 
--- Populate the QUESTIONS table with demo data
--- You should add at least 9 unique questions and their respective options and answers
 INSERT INTO QUESTIONS (TEXT, OPTIONS, ANSWER)
 VALUES ('What is 2 + 2?', '3,4,5,6', '4'),
        ('What is the capital of France?', 'London,Berlin,Paris,Madrid', 'Paris'),
@@ -69,7 +56,6 @@ VALUES ('What is 2 + 2?', '3,4,5,6', '4'),
         'Albert Einstein'),
        ('What is the chemical symbol for gold?', 'Go,Au,Ag,Fe', 'Au');
 
--- Populate the SELECTOR table with demo data to associate questions with quizzes
 INSERT INTO SELECTOR (QUIZ_ID, QUESTION_ID)
 VALUES (1, 1),
        (1, 4),
@@ -81,8 +67,6 @@ VALUES (1, 1),
        (3, 6),
        (3, 9);
 
--- Populate the RESULTS table with demo data
--- You can generate random scores for the results
 INSERT INTO RESULTS (USER_ID, QUIZ_ID, SCORE)
 VALUES (1, 2, 1),
        (1, 3, 2),
