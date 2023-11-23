a921e4413decec4ea266b2f38a478964a6f0de5a
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-23 23:41:36 +0100

    Modified to use JPA.

--- src/main/java/se/kth/id1212/integration/ResultDAOImpl.java
+++ src/main/java/se/kth/id1212/integration/ResultDAOImpl.java
@@ -1,21 +1,20 @@
 package se.kth.id1212.integration;
 
+import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
 import se.kth.id1212.model.Result;
 import se.kth.id1212.model.ResultDAO;
-import se.kth.id1212.util.ExceptionLogger;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * This class provides methods to retrieve all quiz results for a user
  * and to add a new quiz result to the database.
  */
-public class ResultDAOImpl implements ResultDAO< Result > {
-    private final Connection connection = DatabaseHandler.connect();
+public class ResultDAOImpl implements ResultDAO<Result> {
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ResultsPU");
+    EntityManager entityManager;
 
 
     /**
@@ -25,85 +24,71 @@ public class ResultDAOImpl implements ResultDAO< Result > {
      * @return A HashMap containing quiz IDs as keys and corresponding Result objects as values.
      */
     @Override
-    public HashMap< Integer, Result > getAllResults(Integer userID) {
-        HashMap< Integer, Result > results = new HashMap<>();
-        String query = "SELECT * FROM RESULTS WHERE USER_ID = ?";
+    public HashMap<Integer, Result> getAllResults(Integer userID) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        HashMap<Integer, Result> results = new HashMap<>();
 
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, userID);
-            ResultSet resultSet = statement.executeQuery();
+        try {
+            String jpql = "SELECT r FROM Result r WHERE r.userID = :userId";
+            Query query = entityManager.createQuery(jpql, Result.class);
+            query.setParameter("userId", userID);
 
-            while (resultSet.next()) {
-                int quizId = resultSet.getInt("QUIZ_ID");
-                int score = resultSet.getInt("SCORE");
-                Result result = new Result(userID, quizId, score);
-                results.put(quizId, result);
+            List<Result> resultList = query.getResultList();
+
+            for (Result result : resultList) {
+                results.put(result.getQuizID(), result);
             }
-        } catch (SQLException exception) {
-            ExceptionLogger.log(exception);
+        } finally {
+            entityManager.close();
         }
 
         return results;
     }
 
-    /**
-     * Adds or updates quiz results for a specific user in the database.
-     *
-     * @param result The Result object containing the quiz results to add or update.
-     */
-    @Override
+    @Transactional
     public void addResult(Result result) {
-        Integer userId = result.userID();
-        Integer quizId = result.quizID();
-        Integer points = result.score();
+        this.entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
 
+        Integer userId = result.getUserID();
+        Integer quizId = result.getQuizID();
+        Integer points = result.getScore();
+
+        String query = "SELECT r FROM Result r WHERE r.userID = :userId AND r.quizID = :quizId";
+        Query checkQuery = entityManager.createQuery(query, Result.class);
+        checkQuery.setParameter("userId", userId);
+        checkQuery.setParameter("quizId", quizId);
+
+
+        Result existingResult;
         try {
-            String checkQuery = "SELECT ID FROM RESULTS WHERE USER_ID = ? AND QUIZ_ID = ?";
-            PreparedStatement checkStatement = connection.prepareStatement(checkQuery);
-            checkStatement.setInt(1, userId);
-            checkStatement.setInt(2, quizId);
-            ResultSet resultSet = checkStatement.executeQuery();
-
-            if (resultSet.next()) {
-                updateResult(resultSet, points);
-            } else {
-                insertResult(userId, quizId, points);
-            }
-        } catch (SQLException exception) {
-            ExceptionLogger.log(exception);
+            transaction.begin();
+            existingResult = (Result) checkQuery.getSingleResult();
+        } catch (NoResultException e) {
+            existingResult = null;
         }
+
+        if (existingResult != null) {
+            updateResult(existingResult, points);
+        } else {
+            insertResult(userId, quizId, points);
+        }
+        transaction.commit();
+        entityManager.close();
     }
 
-    /**
-     * Updates an existing result in the database with the given points.
-     *
-     * @param resultSet The ResultSet containing the existing result's information.
-     * @param points    The new score to update in the database.
-     * @throws SQLException If a SQL exception occurs during the update process.
-     */
-    private void updateResult(ResultSet resultSet, Integer points) throws SQLException {
-        int existingRecordId = resultSet.getInt("ID");
-        String updateQuery = "UPDATE RESULTS SET SCORE = ? WHERE ID = ?";
-        PreparedStatement updateStatement = connection.prepareStatement(updateQuery);
-        updateStatement.setInt(1, points);
-        updateStatement.setInt(2, existingRecordId);
-        updateStatement.executeUpdate();
+    @Transactional
+    private void updateResult(Result existingResult, Integer points) {
+        existingResult.setScore(points);
+        this.entityManager.merge(existingResult);
     }
 
-    /**
-     * Inserts a new result for a user and quiz with the given points into the database.
-     *
-     * @param userID The unique identifier of the user.
-     * @param quizID The unique identifier of the quiz.
-     * @param points The score achieved in the quiz.
-     * @throws SQLException If a SQL exception occurs during the insertion process.
-     */
-    private void insertResult(Integer userID, Integer quizID, Integer points) throws SQLException {
-        String insertQuery = "INSERT INTO RESULTS (USER_ID, QUIZ_ID, SCORE) VALUES (?, ?, ?)";
-        PreparedStatement insertStatement = connection.prepareStatement(insertQuery);
-        insertStatement.setInt(1, userID);
-        insertStatement.setInt(2, quizID);
-        insertStatement.setInt(3, points);
-        insertStatement.executeUpdate();
+    @Transactional
+    private void insertResult(Integer userID, Integer quizID, Integer points) {
+        Result newResult = new Result();
+        newResult.setUserID(userID);
+        newResult.setQuizID(quizID);
+        newResult.setScore(points);
+        this.entityManager.persist(newResult);
     }
 }
