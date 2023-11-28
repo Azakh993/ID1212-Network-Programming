336cebe0d0eac23f3f8fc6669dfeba2c2c5f68e2
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-28 18:21:05 +0100

    Extracted constants that are better suited in a properties file.

--- SpringMVC-Quiz/src/main/java/se/kth/id1212/springquiz/config/DatabaseInitializer.java
+++ SpringMVC-Quiz/src/main/java/se/kth/id1212/springquiz/config/DatabaseInitializer.java
@@ -14,27 +14,50 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class DatabaseInitializer {
-    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(
-            "QuizPersistenceUnit");
+    private static final EntityManagerFactory entityManagerFactory;
+    private static final String DATABASE_DRIVER;
+    private static final String DATABASE_DIRECTORY;
+    private static final String DATABASE_FILE;
+    private static final String DATABASE_CONNECTION_PREFIX;
+    private static final String SQL_SCRIPT;
+
+
+    static {
+        try {
+            Properties properties = new Properties();
+            properties.load(DatabaseInitializer.class.getClassLoader().getResourceAsStream("database.properties"));
+
+            String persistenceUnitName = properties.getProperty("persistence.unit.name");
+            entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnitName);
+
+            DATABASE_DRIVER = properties.getProperty("db.driver");
+            DATABASE_DIRECTORY = properties.getProperty("db.directory");
+            DATABASE_FILE = properties.getProperty("db.file");
+            DATABASE_CONNECTION_PREFIX = properties.getProperty("db.connection.prefix");
+            SQL_SCRIPT = properties.getProperty("sql.script.path");
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load database properties", e);
+        }
+    }
 
     public static EntityManager getEntityManager() {
         return entityManagerFactory.createEntityManager();
     }
 
-    public static void initializeDatabase() throws ClassNotFoundException {
-        Class.forName("org.apache.derby.iapi.jdbc.AutoloadedDriver");
-        Path filePath = Paths.get("SpringMVC-Quiz", "src", "main", "database", "db.lck");
-        File databaseFile = new File(String.valueOf(filePath.toAbsolutePath()));
+    public static void initializeDatabase() {
+        Path filePath = Paths.get(DATABASE_FILE).toAbsolutePath();
+        File databaseFile = new File(String.valueOf(filePath));
 
-        Path dbDirectoryPath = Paths.get("SpringMVC-Quiz", "src", "main", "database");
-        File dbDirectory = new File(String.valueOf(dbDirectoryPath.toAbsolutePath()));
+        Path dbDirectoryPath = Paths.get(DATABASE_DIRECTORY).toAbsolutePath();
+        File dbDirectory = new File(String.valueOf(dbDirectoryPath));
 
         if (databaseFile.exists()) {
             try {
-                Class.forName("org.apache.derby.iapi.jdbc.AutoloadedDriver");
-                DriverManager.getConnection("jdbc:derby:" + dbDirectory);
+                Class.forName(DATABASE_DRIVER);
+                DriverManager.getConnection(DATABASE_CONNECTION_PREFIX + dbDirectory);
             } catch (ClassNotFoundException | SQLException exception) {
                 ExceptionLogger.log(exception);
             }
@@ -44,24 +67,19 @@ public class DatabaseInitializer {
     }
 
     private static void createNewDatabase(File dbDirectory) {
-
         try {
-            Class.forName("org.apache.derby.iapi.jdbc.AutoloadedDriver");
-
-            Connection connection = DriverManager.getConnection("jdbc:derby:" + dbDirectory.getAbsolutePath()
+            Class.forName(DATABASE_DRIVER);
+            Connection connection = DriverManager.getConnection(DATABASE_CONNECTION_PREFIX + dbDirectory
                     + ";create=true");
 
-            Path scriptPath =
-                    Paths.get("SpringMVC-Quiz", "src", "main", "resources", "sql", "PopulateDB.sql").toAbsolutePath();
+            Path scriptPath = Paths.get(SQL_SCRIPT).toAbsolutePath();
 
             String scriptContent = new String(Files.readAllBytes(scriptPath));
             executeSqlScript(connection, scriptContent);
 
         } catch (SQLException exception) {
             ExceptionLogger.log(exception);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
