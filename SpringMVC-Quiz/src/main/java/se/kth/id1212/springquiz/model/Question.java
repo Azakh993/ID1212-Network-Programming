cfd99f40efca851a81a93a1e9627bb1253c7116f
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-28 04:17:37 +0100

    Reused and modified class; now entity for use with JPA.

---
+++ SpringMVC-Quiz/src/main/java/se/kth/id1212/springquiz/model/Question.java
@@ -0,0 +1,62 @@
+package se.kth.id1212.springquiz.model;
+
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "QUESTIONS")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "TEXT", nullable = false)
+    private String text;
+
+    @Column(name = "OPTIONS", nullable = false)
+    private String options;
+
+    @Column(name = "ANSWER", nullable = false)
+    private String answer;
+
+    @OneToMany(mappedBy = "question")
+    private List<Selector> selectors;
+
+
+    public Question() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getOptions() {
+        return options;
+    }
+
+    public void setOptions(String options) {
+        this.options = options;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
