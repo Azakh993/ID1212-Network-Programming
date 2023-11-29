3f35fa003ae72088dc35f070fc40e61d3a285a5d
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-29 21:02:03 +0100

    Handle exception in method instead.

--- SpringMVC-Quiz/src/main/java/se/kth/id1212/springquiz/service/EmailService.java
+++ SpringMVC-Quiz/src/main/java/se/kth/id1212/springquiz/service/EmailService.java
@@ -6,6 +6,7 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
+import se.kth.id1212.springquiz.util.ExceptionLogger;
 
 import java.util.Properties;
 
@@ -25,7 +26,7 @@ public class EmailService {
     @Value("${mail.smtp.password}")
     private String password;
 
-    public void sendEmail(String to, String subject, String body) throws MessagingException {
+    public void sendEmail(String to, String subject, String body) {
         Properties properties = new Properties();
         properties.put("mail.smtp.host", smtpHost);
         properties.put("mail.smtp.port", smtpPort);
@@ -39,11 +40,15 @@ public class EmailService {
         });
 
         Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(username));
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-        message.setSubject(subject);
-        message.setText(body);
+        try {
+            message.setFrom(new InternetAddress(username + "@kth.se"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(body);
 
-        Transport.send(message);
+            Transport.send(message);
+        } catch (MessagingException exception) {
+            ExceptionLogger.log(exception);
+        }
     }
 }
