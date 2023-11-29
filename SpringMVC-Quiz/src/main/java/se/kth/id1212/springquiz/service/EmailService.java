e04dd78c4b08649e88318ff38a5ab486ba2b151c
Azmeer K. <azmeer.khawaja@outlook.com>
2023-11-29 16:39:47 +0100

    Coded for sending email containing result of a quiz.

---
+++ SpringMVC-Quiz/src/main/java/se/kth/id1212/springquiz/service/EmailService.java
@@ -0,0 +1,49 @@
+package se.kth.id1212.springquiz.service;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+@Service
+@PropertySource("classpath:email.properties")
+public class EmailService {
+
+    @Value("${mail.smtp.host}")
+    private String smtpHost;
+
+    @Value("${mail.smtp.port}")
+    private String smtpPort;
+
+    @Value("${mail.smtp.username}")
+    private String username;
+
+    @Value("${mail.smtp.password}")
+    private String password;
+
+    public void sendEmail(String to, String subject, String body) throws MessagingException {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", smtpHost);
+        properties.put("mail.smtp.port", smtpPort);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(username));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+        message.setSubject(subject);
+        message.setText(body);
+
+        Transport.send(message);
+    }
+}
