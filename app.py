7fda0b81e0d76fd9707170445fe7610cac357e75
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-08 14:39:01 +0100

    Refactored: adjustment due to renaming of login controller.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, session, request
 from config.config import SECRET_KEY
-from controllers import login_controller
+from controllers import login
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
@@ -22,11 +22,11 @@ def set_course(course_code):
 def show_login_page(course_code):
     if course_code_set(course_code):
         if request.method == "GET":
-            return login_controller.show_login_page(course_code)
+            return login.show_login_page(course_code)
         elif request.method == "POST":
             username = request.form['username']
             password = request.form['password']
-            return login_controller.login(username, password)
+            return login.login(username, password)
     return redirect(url_for('show_login_page', course_code=course_code))
 
 
