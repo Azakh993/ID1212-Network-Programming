8fe89ee442041113b21ebedf49fdb3db74e68b40
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-08 20:54:10 +0100

    Enhancement: added Course entity.

---
+++ models/course.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, VARCHAR
+from sqlalchemy.orm import relationship
+
+from controllers import Base
+
+
+class Course(Base):
+    __tablename__ = 'courses'
+
+    id = Column(VARCHAR, primary_key=True)
+    name = Column(VARCHAR, nullable=False)
+
+    registrations = relationship('UserCourseRegistration', back_populates='course')
