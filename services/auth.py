e1f32e21f366b7b68e1a86f4771c706619d57d1a
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-08 20:51:32 +0100

    Refactor: course code passed to method for getting user by username.

--- services/auth.py
+++ services/auth.py
@@ -1,8 +1,8 @@
 from repositories.user import get_user_by_username
 
 
-def authenticate_user(username, password):
-    user = get_user_by_username(username)
+def authenticate_user(course_code, username, password):
+    user = get_user_by_username(course_code, username)
 
     if user is not None and user.password == password:
         return user
