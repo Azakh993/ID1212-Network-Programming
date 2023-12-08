cb6bafd22ccc2f0b7f9f5c79598d13e3fb85c7e8
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-08 20:55:30 +0100

    Enhancement: added code for connecting User with UserCourseRegistration entity.

--- models/user.py
+++ models/user.py
@@ -1,10 +1,15 @@
-from sqlalchemy import Column, Integer, VARCHAR
+from sqlalchemy import Column, INTEGER, VARCHAR, BOOLEAN
+from sqlalchemy.orm import relationship
+
 from controllers import Base
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(INTEGER, primary_key=True)
     username = Column(VARCHAR, unique=True, nullable=False)
     password = Column(VARCHAR, unique=False, nullable=False)
+    admin = Column(BOOLEAN, nullable=False)
+
+    registrations = relationship('UserCourseRegistration', back_populates='user')
