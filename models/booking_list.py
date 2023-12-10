12f22bc917eb4601527f5c2ff461f6530dea444d
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-10 17:55:53 +0100

    Added entity representing a booking list.

---
+++ models/booking_list.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, INTEGER, VARCHAR, TIMESTAMP, ForeignKey
+from sqlalchemy.orm import relationship
+
+from controllers import Base
+
+
+class BookingList(Base):
+    __tablename__ = 'booking_lists'
+
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
+    course_id = Column(VARCHAR, ForeignKey('courses.id'), nullable=False)
+    description = Column(VARCHAR, nullable=False)
+    location = Column(VARCHAR, nullable=False)
+    time = Column(TIMESTAMP, nullable=False)
+    interval = Column(INTEGER, nullable=False)
+    max_slots = Column(INTEGER, nullable=False)
+
+    course = relationship('Course', back_populates='booking_lists')
