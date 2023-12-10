6079e8b98d86f724f50414423311737ed6081e57
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-10 21:37:51 +0100

    Added methods for validating data in a given booking list dto, sending booking lists to repository and generating json:ified booking lists.

--- services/booking_list_service.py
+++ services/booking_list_service.py
@@ -1,6 +1,7 @@
 import copy
 
-from repositories.booking_list_repository import retrieve_booking_lists
+from models.booking_list import BookingList
+from repositories.booking_list_repository import retrieve_booking_lists, insert_booking_list
 
 
 def get_booking_lists(course_code):
@@ -21,3 +22,44 @@ def remove_seconds_from_booking_lists(booking_lists):
         list_copy.time = list_copy.time.strftime('%Y-%m-%d %H:%M')
         booking_lists_without_seconds.append(list_copy)
     return booking_lists_without_seconds
+
+
+def add_booking_list(course_code, booking_list_dto):
+    new_booking_list = BookingList(
+        course_id=course_code.upper(),
+        description=booking_list_dto.description,
+        location=booking_list_dto.location,
+        time=booking_list_dto.time,
+        interval=booking_list_dto.interval,
+        max_slots=booking_list_dto.max_slots
+    )
+
+    try:
+        insert_booking_list(new_booking_list)
+    except Exception as exception:
+        print(f'Error: {str(exception)}')
+        return False
+    return True
+
+
+def is_invalid_booking_list(booking_list_dto):
+    return any(value is None for value in (
+        booking_list_dto.description,
+        booking_list_dto.location,
+        booking_list_dto.time,
+        booking_list_dto.interval,
+        booking_list_dto.max_slots
+    ))
+
+
+def generate_json_booking_lists(course_code):
+    booking_lists = get_booking_lists(course_code)
+    json_booking_lists = [{
+        "description": booking_list.description,
+        "location": booking_list.location,
+        "time": booking_list.time,
+        "interval": booking_list.interval,
+        "max_slots": booking_list.max_slots
+    } for booking_list in booking_lists]
+
+    return json_booking_lists
