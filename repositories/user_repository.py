4b1772a17f712619553811754ded16ba39834291
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-10 17:24:39 +0100

    Bugfix: repo has upper case course codes, so string for filtering needs to be the same.

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -6,9 +6,9 @@ from repositories import session
 def get_user_by_username(course_code, username):
     try:
         return (((session.query(User)
-                .join(UserCourseRegistration))
-                .filter(UserCourseRegistration.course_id == course_code,
-                        User.username == username))
+                  .join(UserCourseRegistration))
+                 .filter(UserCourseRegistration.course_id == course_code.upper(),
+                         User.username == username))
                 .first())
     except Exception as exception:
         print(f'Error: {str(exception)}')
@@ -23,4 +23,3 @@ def get_user_by_user_id(user_id):
         print(f'Error: {str(exception)}')
         session.rollback()
         raise
-
