73218786109759e22e3502d1c43af64b8c822d40
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-10 17:26:34 +0100

    Moved admin attribute from User to UserCourseRegistration class.

--- models/registration.py
+++ models/registration.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, VARCHAR, INTEGER, ForeignKey
+from sqlalchemy import Column, VARCHAR, INTEGER, ForeignKey, BOOLEAN
 from sqlalchemy.orm import relationship
 
 from controllers import Base
@@ -9,6 +9,7 @@ class UserCourseRegistration(Base):
 
     user_id = Column(INTEGER, ForeignKey('users.id'), primary_key=True)
     course_id = Column(VARCHAR, ForeignKey('courses.id'), primary_key=True)
+    admin = Column(BOOLEAN, nullable=False)
 
     user = relationship('User', back_populates='registrations')
     course = relationship('Course', back_populates='registrations')
--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, INTEGER, VARCHAR, BOOLEAN
+from sqlalchemy import Column, INTEGER, VARCHAR
 from sqlalchemy.orm import relationship
 
 from controllers import Base
@@ -10,6 +10,5 @@ class User(Base):
     id = Column(INTEGER, primary_key=True)
     username = Column(VARCHAR, unique=True, nullable=False)
     password = Column(VARCHAR, unique=False, nullable=False)
-    admin = Column(BOOLEAN, nullable=False)
 
     registrations = relationship('UserCourseRegistration', back_populates='user')
