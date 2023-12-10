dc799ae580a918a0f7ffaffca38badc43b939181
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-10 21:42:20 +0100

    Added functionality for handling entry of new booking list by first PUT-ting and then re-rendering table content with response data.

--- static/js/booking_lists.js
+++ static/js/booking_lists.js
@@ -8,11 +8,58 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    document.getElementById("newBookingRowSubmitBtn").addEventListener("click", function () {
+        const courseCode = course_code
+        const time = document.getElementById("inputTime").value;
+        const description = document.getElementById("inputDescription").value;
+        const location = document.getElementById("inputLocation").value;
+        const length = document.getElementById("inputLength").value;
+        const slots = document.getElementById("inputSlots").value;
+
+        function updateBookingListUI(json_data) {
+            const tableBody = document.querySelector("#bookingTable tbody");
+            tableBody.innerHTML = "";
+
+            json_data.booking_lists.forEach((booking) => {
+                const newRow = document.createElement("tr");
+                newRow.innerHTML = `
+                        <td><input type="radio" name="selectedBooking" value="${booking.id}"></td>
+                        <td>${booking.time}</td>
+                        <td>${booking.description}</td>
+                        <td>${booking.location}</td>
+                        <td>${booking.interval} min</td>
+                        <td style="text-align: center;">${booking.slots}</td>
+                    `;
+                tableBody.appendChild(newRow);
+            });
+        }
+
+        fetch(`/${courseCode}/booking-lists`, {
+            method: "PUT",
+            body: JSON.stringify({time, description, location, length, slots}),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => {
+                if (response.status === 201) {
+                    return response.json();
+                } else {
+                    alert("Failed to create booking list");
+                }
+            }).then((json_data) => {
+            updateBookingListUI(json_data);
+        })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    });
+
+
     document.getElementById("selectBookingBtn").addEventListener("click", function () {
         const selectedBooking = document.querySelector("input[name='selectedBooking']:checked");
         if (selectedBooking) {
             var selectedBookingId = selectedBooking.value;
-            // Perform an action with the selected booking
             alert("Selected booking ID: " + selectedBookingId);
         } else {
             alert("Please select a booking.");
@@ -20,6 +67,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     document.getElementById("showMyBookingsBtn").addEventListener("click", function () {
+        // Handle showing user's bookings here
+    });
+
+    document.getElementById("removeBookingListBtn").addEventListener("click", function () {
         // Handle showing user's bookings here
     });
 });
