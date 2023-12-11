e5c4f0742f64f6e5fecaf0b44649f375f090830d
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-11 15:58:07 +0100

    Refactored: improved code smell.

--- static/js/login.js
+++ static/js/login.js
@@ -1,34 +1,49 @@
-document.getElementById("login-form").addEventListener("submit", function (event) {
+document.getElementById("login-form").addEventListener("submit", handleLoginFormSubmit);
+
+function handleLoginFormSubmit(event) {
     event.preventDefault();
 
-    const courseCode = course_code
+    const courseCode = course_code;
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const errorMessage = document.getElementById("error-message");
 
-    fetch(`/${courseCode}/login`, {
+    authenticateUser(courseCode, username, password, errorMessage);
+}
+
+function authenticateUser(courseCode, username, password) {
+    const requestOptions = {
         method: "POST",
-        body: new URLSearchParams({username, password}),
+        body: JSON.stringify({username: username, password: password}),
         headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Type": "application/json",
         },
-    })
-        .then((response) => {
-            if (response.status === 200) {
-                return response.json();
-            } else {
-                errorMessage.innerHTML = "<p>Invalid username or password</p>";
-                throw new Error("Authentication failed");
-            }
-        })
-        .then((data) => {
-            if (data.error) {
-                errorMessage.innerHTML = `<p>${data.error}</p>`;
-            } else if (data.success) {
-                window.location.href = `/${courseCode}/booking-lists`;
-            }
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-        });
-});
+    };
+
+    fetch(`/courses/${courseCode}/login`, requestOptions)
+        .then(handleAuthenticationResponse)
+        .catch(handleAuthenticationError);
+}
+
+function handleAuthenticationResponse(response) {
+    if (response.status === 204) {
+        redirectUserAfterAuthentication();
+    } else {
+        displayAuthenticationError();
+    }
+}
+
+function redirectUserAfterAuthentication() {
+    const courseCode = course_code;
+    window.location.href = `/courses/${courseCode}/booking-lists`;
+}
+
+function displayAuthenticationError() {
+    const errorMessage = document.getElementById("error-message");
+    errorMessage.innerHTML = "<p>Invalid username or password</p>";
+    throw new Error("Authentication failed");
+}
+
+function handleAuthenticationError(error) {
+    console.error("Error:", error);
+}
