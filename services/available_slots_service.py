239bdc9d27426746e33723edc9ae52e04a5ade89
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-11 05:06:15 +0100

    Added functionality for generating available slots and json-prepping available_slots.

---
+++ services/available_slots_service.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+
+from repositories.reservation_repository import retrieve_reservations
+
+
+def generate_available_slots(booking_list):
+    available_slots = []
+    max_slots = booking_list.max_slots
+    interval = booking_list.interval
+
+    format_string = "%Y-%m-%d %H:%M"
+    start_time = datetime.strptime(booking_list.time, format_string)
+
+    for i in range(max_slots):
+        available_slots.append(AvailableSlotDTO(booking_list.id, i, start_time))
+        start_time += timedelta(minutes=interval)
+
+    reservations = retrieve_reservations(booking_list.id)
+    if reservations is not None:
+        for reservation in reservations:
+            available_slots[reservation.sequence_id].user_id = reservation.user_id
+
+    return available_slots
+
+
+def generate_json_ready_available_slots(booking_list):
+    available_slots = generate_available_slots(booking_list)
+    json_available_slots = [{
+        "list_id": available_slot.list_id,
+        "sequence_id": available_slot.sequence_id,
+        "start_time": available_slot.start_time,
+        "user_id": available_slot.user_id,
+    } for available_slot in available_slots]
+
+    return json_available_slots
+
+
+class AvailableSlotDTO:
+    def __init__(self, booking_list_id, sequence_id, start_time):
+        self.list_id = booking_list_id
+        self.sequence_id = sequence_id
+        self.start_time = start_time
+        self.user_id = None
