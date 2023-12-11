b64f25c121fb77cb0514aca0debd232a8801bc3e
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-11 21:25:39 +0100

    Fixed formatting of start time of a slot. Also added code for reserving a slot and generating a json-ready reservation.

--- services/reservations_service.py
+++ services/reservations_service.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 
-from repositories.reservation_repository import retrieve_reservations
+from models import Reservation
+from repositories.reservation_repository import retrieve_reservations, insert_reservation
 from repositories.user_repository import get_user_by_user_id
 
 
@@ -13,7 +14,7 @@ def generate_available_slots(booking_list):
     start_time = datetime.strptime(booking_list.time, format_string)
 
     for i in range(max_slots):
-        available_slots.append(AvailableSlotDTO(booking_list.id, i, start_time))
+        available_slots.append(AvailableSlotDTO(booking_list.id, i, start_time.strftime(format_string)))
         start_time += timedelta(minutes=interval)
 
     reservations = retrieve_reservations(booking_list.id)
@@ -37,8 +38,31 @@ def generate_json_ready_available_slots(booking_list):
         "user_id": available_slot.user_id,
         "username": available_slot.username
     } for available_slot in available_slots]
-
     return json_available_slots
+
+
+def generate_json_ready_reservation(reservation):
+    return {
+        "id": reservation.id,
+        "list_id": reservation.list_id,
+        "user_id": reservation.user_id,
+        "sequence_id": reservation.sequence_id
+    }
+
+
+def reserve_slot(user_id, booking_list_id, sequence_id):
+    new_reservation = Reservation(
+        list_id=booking_list_id,
+        user_id=user_id,
+        sequence_id=sequence_id
+    )
+
+    try:
+        insert_reservation(new_reservation)
+    except Exception as exception:
+        print(f'Error: {str(exception)}')
+        return None
+    return new_reservation
 
 
 class AvailableSlotDTO:
