aeb6eecb02857e84027e4d6002fec9e9b6c5305b
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-11 21:24:35 +0100

    Added functionality to persist given reservation in database.

--- repositories/reservation_repository.py
+++ repositories/reservation_repository.py
@@ -1,8 +1,12 @@
 from models.reservation import Reservation
 from repositories import session
-from repositories.repository_util import get_all_from_database
+from repositories.repository_util import get_all_from_database, add_to_database
 
 
 def retrieve_reservations(booking_list_id):
     return get_all_from_database(session.query(Reservation)
                                  .filter_by(list_id=booking_list_id))
+
+
+def insert_reservation(new_reservation):
+    add_to_database(new_reservation)
