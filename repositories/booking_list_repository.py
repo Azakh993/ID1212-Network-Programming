a05e9c0a3e5fc2ea21eb7a065c51fc0846c92c39
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-11 01:07:21 +0100

    Added functionality for removing booking list entry for database table.

--- repositories/booking_list_repository.py
+++ repositories/booking_list_repository.py
@@ -19,3 +19,15 @@ def insert_booking_list(new_booking_list):
         print(f'Error: {str(exception)}')
         session.rollback()
         raise
+
+
+def delete_booking_list(course_code, booking_id):
+    try:
+        (session.query(BookingList)
+         .filter(BookingList.course_id == course_code.upper(), BookingList.id == booking_id)
+         .delete())
+        session.commit()
+    except Exception as exception:
+        print(f'Error: {str(exception)}')
+        session.rollback()
+        raise
