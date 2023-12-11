80c3b2e2902672db53620796e3bd67400d2eb1dc
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-11 15:52:35 +0100

    Refactored to better follow recommended behaviour for request types.

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -9,13 +9,9 @@ def show_login_page(course_code):
 
 def authenticate(course_code, username, password):
     user = authenticate_user(course_code, username, password)
-
     if user is None:
-        rendered_template = render_template("login.html", course_code=course_code)
-        response = make_response(rendered_template)
-        response.status_code = 401
-        return response
+        error_message = {"error": "Unauthorized!"}
+        return make_response(jsonify(error_message), 401)
 
     session['user_id'] = user.id
-    success_response = jsonify({"success": True})
-    return make_response(success_response, 200)
+    return make_response('', 204)
