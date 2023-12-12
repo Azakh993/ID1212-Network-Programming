ef450bba3e36c4f2e25fb10d0c00b8fb669a3e05
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-12 22:17:15 +0100

    Refactored names to avoid confusion.

--- controllers/reservations_controller.py
+++ controllers/reservations_controller.py
@@ -1,6 +1,6 @@
 from flask import request, render_template, make_response, jsonify
 
-from services.reservations_service import generate_user_reservation_entries, remove_user_reservation
+from services.reservations_service import generate_user_reservation_entries, remove_reservation
 
 
 def show_reservations_page(course_code, user_id):
@@ -14,8 +14,8 @@ def show_reservations_page(course_code, user_id):
     return render_template("my_bookings.html", reservations=user_reservation_entries, course_code=course_code)
 
 
-def remove_reservation(reservation_id):
-    successful_removal = remove_user_reservation(reservation_id)
+def remove_user_reservation(reservation_id):
+    successful_removal = remove_reservation(reservation_id)
 
     if successful_removal:
         return make_response(jsonify({}), 204)
