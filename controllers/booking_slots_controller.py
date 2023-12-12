5d902f74d1c16a118d87f7afac4884835c0fe083
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-12 22:11:42 +0100

    Added handling of booking requests when user has pre-existing bookings in booking list.
    Also added functionality for adding reservations for given username instead of user_id, and removal of reservations.

--- controllers/booking_slots_controller.py
+++ controllers/booking_slots_controller.py
@@ -1,10 +1,10 @@
 from flask import request, render_template, make_response, jsonify
 
 from controllers.controller_util import check_privileges
-from services.auth_service import get_user_privileges
+from services.auth_service import get_user_privileges, retrieve_user_by_username
 from services.booking_list_service import get_booking_list
 from services.reservations_service import generate_available_slots, generate_json_ready_available_slots, reserve_slot, \
-    generate_json_ready_reservation
+    generate_json_ready_reservation, remove_slot_reservation, get_user_reservations_for_booking_list
 
 
 def show_slots_page(course_code, user_id, booking_id):
@@ -35,7 +35,9 @@ def render_slots_page(course_code, booking_list, user_privileges, available_slot
 
 
 def book_slot(course_code, user_id, booking_id, slot_id):
-    check_privileges(course_code, user_id)
+    existing_reservations = check_for_other_reservations(user_id, booking_id)
+    if existing_reservations:
+        return send_error_response(403)
 
     booking_list = get_booking_list(booking_id)
     available_slots = generate_available_slots(booking_list)
@@ -44,6 +46,15 @@ def book_slot(course_code, user_id, booking_id, slot_id):
     if requested_slot_to_book.user_id is not None:
         return make_response(jsonify({"error": "Slot is already booked"}), 400)
 
+    user_to_book = request.get_json().get("username")
+    if user_to_book:
+        check_privileges(course_code, user_id)
+        valid_user = retrieve_user_by_username(course_code, user_to_book)
+        if valid_user:
+            user_id = valid_user.id
+        else:
+            return send_error_response(404)
+
     added_reservation = reserve_slot(user_id, booking_id, slot_id)
 
     if added_reservation:
@@ -51,9 +62,23 @@ def book_slot(course_code, user_id, booking_id, slot_id):
         response_data = {"newReservation": json_ready_reservation}
         return make_response(jsonify(response_data), 201)
     else:
-        return send_error_response()
+        return send_error_response(400)
 
 
-def send_error_response():
+def check_for_other_reservations(user_id, booking_list_id):
+    return get_user_reservations_for_booking_list(user_id, booking_list_id)
+
+
+def remove_reservation(course_code, user_id, booking_list_id, slot_sequence_id):
+    check_privileges(course_code, user_id)
+    successful_removal = remove_slot_reservation(booking_list_id, slot_sequence_id)
+
+    if successful_removal:
+        return make_response(jsonify({}), 204)
+    else:
+        return send_error_response(404)
+
+
+def send_error_response(status_code):
     error_message = {"error": "Reservation could not be added."}
-    return make_response(jsonify(error_message), 400)
+    return make_response(jsonify(error_message), status_code)
