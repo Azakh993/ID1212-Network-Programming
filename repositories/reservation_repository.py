7997a2b93fa30e16c81c517d7eab20100d7a3ba3
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-12 22:16:54 +0100

    Added code for retrieving reservations using user id and booking list id as parameters. Also added code for deleting a reservation.

--- repositories/reservation_repository.py
+++ repositories/reservation_repository.py
@@ -1,7 +1,8 @@
 from models import BookingList, User
 from models.reservation import Reservation
 from repositories import session
-from repositories.repository_util import get_all_from_database, add_to_database, delete_from_database
+from repositories.repository_util import get_all_from_database, add_to_database, delete_from_database, \
+    get_first_from_database
 
 
 def retrieve_reservations(booking_list_id):
@@ -20,5 +21,16 @@ def retrieve_user_reservations(course_code, user_id):
                                           User.id == user_id)))
 
 
+def retrieve_user_reservations_by_booking_list_id(user_id, booking_list_id):
+    return get_first_from_database(
+        session.query(Reservation).filter(Reservation.user_id == user_id, Reservation.list_id == booking_list_id))
+
+
 def delete_reservation(reservation_id):
     delete_from_database(session.query(Reservation).filter(Reservation.id == reservation_id))
+
+
+def delete_slot_reservation(booking_list_id, slot_sequence_id):
+    delete_from_database(session.query(Reservation)
+                         .filter(Reservation.list_id == booking_list_id,
+                                 Reservation.sequence_id == slot_sequence_id))
