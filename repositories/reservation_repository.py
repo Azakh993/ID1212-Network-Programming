c70c1c51fa8a46d879629352d789bbd9be801e19
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-12 01:37:42 +0100

    Added functionality of retrieving reservations associated to specific user (in given course), as well as delete a specific reservation.

--- repositories/reservation_repository.py
+++ repositories/reservation_repository.py
@@ -1,6 +1,7 @@
+from models import BookingList, User
 from models.reservation import Reservation
 from repositories import session
-from repositories.repository_util import get_all_from_database, add_to_database
+from repositories.repository_util import get_all_from_database, add_to_database, delete_from_database
 
 
 def retrieve_reservations(booking_list_id):
@@ -10,3 +11,14 @@ def retrieve_reservations(booking_list_id):
 
 def insert_reservation(new_reservation):
     add_to_database(new_reservation)
+
+
+def retrieve_user_reservations(course_code, user_id):
+    return get_all_from_database(((session.query(Reservation)
+                                   .join(BookingList, BookingList.id == Reservation.list_id))
+                                  .filter(BookingList.course_id == course_code.upper(),
+                                          User.id == user_id)))
+
+
+def delete_reservation(reservation_id):
+    delete_from_database(session.query(Reservation).filter(Reservation.id == reservation_id))
