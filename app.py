2e1e5ac705a1eb53d718212cbdfd03a02d053696
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-13 02:29:59 +0100

    Added code to setup event handlers on SocketIO for relevant pages.

--- app.py
+++ app.py
@@ -7,26 +7,37 @@ from controllers.booking_list_controller import manage_booking_lists, \
     delete_booking_list
 from controllers.booking_slots_controller import show_booking_slots, manage_booking_slots
 from controllers.reservations_controller import user_reservations
+from controllers.websocket_controller import emit_update_booking_lists, emit_update_booking_slots, \
+    emit_update_personal_bookings
+
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 socketio = SocketIO(app)
 
+socketio.on_event('booking_lists_changed', emit_update_booking_lists)
+socketio.on_event('booking_slots_changed', emit_update_booking_slots)
+socketio.on_event('personal_bookings_changed', emit_update_personal_bookings)
+
 app.add_url_rule("/courses/<course_code>/login",
-                 view_func=login, methods=["GET", "POST"])
-
+                 view_func=login,
+                 methods=["GET", "POST"])
 app.add_url_rule("/courses/<course_code>/booking-lists",
-                 view_func=manage_booking_lists, methods=["GET", "POST"])
+                 view_func=manage_booking_lists,
+                 methods=["GET", "POST"])
 app.add_url_rule("/courses/<course_code>/booking-lists/<booking_list_id>",
-                 view_func=delete_booking_list, methods=["DELETE"])
-
+                 view_func=delete_booking_list,
+                 methods=["DELETE"])
 app.add_url_rule("/courses/<course_code>/booking-lists/<booking_list_id>/bookable-slots",
-                 view_func=show_booking_slots, methods=["GET"])
+                 view_func=show_booking_slots,
+                 methods=["GET"])
 app.add_url_rule("/courses/<course_code>/booking-lists/<booking_list_id>/bookable-slots/<sequence_id>",
-                 view_func=manage_booking_slots, methods=["POST", "DELETE"])
-
+                 view_func=manage_booking_slots,
+                 methods=["POST", "DELETE"])
 app.add_url_rule("/courses/<course_code>/my-bookings",
-                 view_func=user_reservations, methods=["GET", "DELETE"])
+                 view_func=user_reservations,
+                 methods=["GET", "DELETE"])
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
