2696a0fc5c30d1efca8bf93401dd8b5cdb88b48f
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-16 15:14:19 +0100

    Bugfix: no more infinite loops. Also triggers updates to other pages that are affected.

--- controllers/websocket_controller.py
+++ controllers/websocket_controller.py
@@ -2,12 +2,16 @@ from flask_socketio import emit
 
 
 def emit_update_booking_lists():
-    emit('update_booking_list', broadcast=True)
-
-
-def emit_update_booking_slots():
+    emit('update_booking_list', broadcast=True, include_self=False)
     emit('update_booking_slots', broadcast=True)
 
 
-def emit_update_personal_bookings():
+def emit_update_booking_slots():
+    emit('update_booking_slots', broadcast=True, include_self=False)
+    emit('update_booking_list', broadcast=True)
     emit('update_personal_bookings', broadcast=True)
+
+
+def emit_update_personal_bookings():
+    emit('update_booking_list', broadcast=True)
+    emit('update_booking_slots', broadcast=True)
