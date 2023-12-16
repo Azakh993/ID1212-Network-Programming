9006c1968d0f1e6ab6c8fe95816298e784ebca0a
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-16 22:48:01 +0100

    Added new event handlers for when a booking list is either removed or added.

--- controllers/websocket_controller.py
+++ controllers/websocket_controller.py
@@ -1,6 +1,15 @@
 from flask_socketio import emit
 
 
+def emit_get_new_booking_list(booking_list):
+    emit('new_booking_list_added', booking_list, broadcast=True, include_self=False)
+
+
+def emit_remove_existing_booking_list(booking_list_id):
+    emit('existing_booking_list_removed', booking_list_id, broadcast=True, include_self=False)
+    emit('update_booking_slots', broadcast=True)
+
+
 def emit_update_booking_lists():
     emit('update_booking_list', broadcast=True, include_self=False)
     emit('update_booking_slots', broadcast=True)
