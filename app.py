cd844ff7cb172476e48cd239c46065754af15ec0
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-16 14:44:35 +0100

    Added routing for cases when an invalid course code is given (does not exist in DB). Refactored.

--- app.py
+++ app.py
@@ -2,42 +2,45 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 from config.config import SECRET_KEY
-from controllers.auth_controller import login
-from controllers.booking_list_controller import manage_booking_lists, \
-    delete_booking_list
-from controllers.booking_slots_controller import show_booking_slots, manage_booking_slots
-from controllers.reservations_controller import user_reservations
-from controllers.websocket_controller import emit_update_booking_lists, emit_update_booking_slots, \
-    emit_update_personal_bookings
-
+from controllers import auth_controller as auth_ctrl
+from controllers import booking_list_controller as bl_ctrl
+from controllers import booking_slots_controller as bs_ctrl
+from controllers import error_page_controller as ep_ctrl
+from controllers import reservations_controller as r_ctrl
+from controllers import websocket_controller as s_ctrl
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 socketio = SocketIO(app)
 
-socketio.on_event('booking_lists_changed', emit_update_booking_lists)
-socketio.on_event('booking_slots_changed', emit_update_booking_slots)
-socketio.on_event('personal_bookings_changed', emit_update_personal_bookings)
+socketio.on_event('booking_lists_changed', s_ctrl.emit_update_booking_lists)
+socketio.on_event('booking_slots_changed', s_ctrl.emit_update_booking_slots)
+socketio.on_event('personal_bookings_changed', s_ctrl.emit_update_personal_bookings)
 
 app.add_url_rule("/courses/<course_code>/login",
-                 view_func=login,
+                 view_func=auth_ctrl.login,
+                 methods=["GET", "POST"])
+app.add_url_rule("/courses/<course_code>/add-users",
+                 view_func=auth_ctrl.add_users,
                  methods=["GET", "POST"])
 app.add_url_rule("/courses/<course_code>/booking-lists",
-                 view_func=manage_booking_lists,
+                 view_func=bl_ctrl.manage_booking_lists,
                  methods=["GET", "POST"])
 app.add_url_rule("/courses/<course_code>/booking-lists/<booking_list_id>",
-                 view_func=delete_booking_list,
+                 view_func=bl_ctrl.remove_booking_list,
                  methods=["DELETE"])
 app.add_url_rule("/courses/<course_code>/booking-lists/<booking_list_id>/bookable-slots",
-                 view_func=show_booking_slots,
+                 view_func=bs_ctrl.show_booking_slots,
                  methods=["GET"])
 app.add_url_rule("/courses/<course_code>/booking-lists/<booking_list_id>/bookable-slots/<sequence_id>",
-                 view_func=manage_booking_slots,
+                 view_func=bs_ctrl.manage_booking_slots,
                  methods=["POST", "DELETE"])
 app.add_url_rule("/courses/<course_code>/my-bookings",
-                 view_func=user_reservations,
+                 view_func=r_ctrl.user_reservations,
                  methods=["GET", "DELETE"])
-
+app.add_url_rule("/invalid_course_code",
+                 view_func=ep_ctrl.show_invalid_course_code_page,
+                 methods=["GET"])
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, log_output=True, use_reloader=True)
