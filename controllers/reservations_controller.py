5c5825276abd4173b0b6f53a18ec8470397ab708
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-16 15:08:46 +0100

    Refactored to be more clean and reuse more code.

--- controllers/reservations_controller.py
+++ controllers/reservations_controller.py
@@ -1,35 +1,27 @@
-from flask import request, render_template, make_response, jsonify, session
+from flask import request, render_template, session
 
-from controllers.controller_util import validate_course_code, validate_user_login
-from services.reservations_service import generate_user_reservation_entries, remove_reservation
+from services import reservations_service as rs
+from util import utility as util
 
 
-@validate_course_code
-@validate_user_login
+@util.validate_course_code
+@util.validate_user_login
 def user_reservations(course_code):
     if request.method == "GET":
         return show_reservations_page(course_code, session.get("user_id"))
 
     if request.method == "DELETE":
-        reservation_id = request.get_json().get("reservation_id")
-        return remove_user_reservation(reservation_id)
+        return remove_user_reservation(request.get_json().get("reservation_id"))
 
 
 def show_reservations_page(course_code, user_id):
-    user_reservation_entries = generate_user_reservation_entries(course_code, user_id)
+    user_reservation_entries = rs.generate_user_reservation_entries(course_code, user_id)
 
-    json_request = request.headers.get('Accept') == 'application/json'
-    if json_request:
-        response_data = {"reservations": user_reservation_entries}
-        return make_response(jsonify(response_data), 200)
+    if util.json_request():
+        return util.send_response(util.HTTP_200_OK, {"reservations": user_reservation_entries})
 
     return render_template("my_bookings.html", reservations=user_reservation_entries, course_code=course_code)
 
 
 def remove_user_reservation(reservation_id):
-    successful_removal = remove_reservation(reservation_id)
-
-    if successful_removal:
-        return make_response(jsonify({}), 204)
-
-    return make_response(jsonify({"error": "Reservation not found"}), 404)
+    return util.send_response(*rs.remove_reservation(reservation_id))
