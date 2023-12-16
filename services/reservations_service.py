651fbcbd6c9e0d667045977ebca65626f9947712
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-16 15:11:05 +0100

    Refactored for better code smell.

--- services/reservations_service.py
+++ services/reservations_service.py
@@ -1,39 +1,77 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from models import Reservation
-from repositories.booking_list_repository import retrieve_booking_list
-from repositories.reservation_repository import (retrieve_reservations, insert_reservation, retrieve_user_reservations,
-                                                 delete_reservation, delete_slot_reservation,
-                                                 retrieve_user_reservations_by_booking_list_id)
-from repositories.user_repository import get_user_by_user_id
+from repositories import booking_list_repository as blr
+from repositories import reservation_repository as rr
+from repositories import user_repository as ur
+from util import utility as util
 
 
-def generate_available_slots(booking_list):
-    available_slots = []
-    max_slots = booking_list.max_slots
-    interval = booking_list.interval
+class AvailableSlotDTO:
+    def __init__(self, booking_list_id, sequence_id, start_time):
+        self.list_id = booking_list_id
+        self.sequence_id = sequence_id
+        self.start_time = start_time
+        self.user_id = None
+        self.username = None
 
-    start_time = datetime.strptime(booking_list.time, "%Y-%m-%d %H:%M")
 
-    for i in range(max_slots):
-        string_date_time = format_datetime(start_time)
-        available_slots.append(AvailableSlotDTO(booking_list.id, i, string_date_time))
-        start_time += timedelta(minutes=interval)
+def generate_available_slots(booking_list_id):
+    booking_list = blr.retrieve_booking_list(booking_list_id)
 
-    reservations = retrieve_reservations(booking_list.id)
+    available_slots = [AvailableSlotDTO(
+        booking_list_id=booking_list.id,
+        start_time=process_datetime(booking_list.time, booking_list.interval, i),
+        sequence_id=i) for i in range(booking_list.max_slots)]
+
+    reservations = rr.retrieve_reservations(booking_list.id)
     if reservations is not None:
         for reservation in reservations:
-            user_id = reservation.user_id
-            username = get_user_by_user_id(user_id).username
-
-            available_slots[reservation.sequence_id].user_id = user_id
-            available_slots[reservation.sequence_id].username = username
+            available_slots[reservation.sequence_id].user_id = reservation.user_id
+            available_slots[reservation.sequence_id].username = ur.get_user_by_user_id(reservation.user_id).username
 
     return available_slots
 
 
-def generate_json_ready_available_slots(booking_list):
-    available_slots = generate_available_slots(booking_list)
+def book_slot(course_code, user_id, booking_list_id, slot_id, username):
+    requested_slot = get_requested_slot(booking_list_id, slot_id)
+    if not slot_is_available(requested_slot):
+        return util.send_response(util.HTTP_422_UNPROCESSABLE_ENTITY, {"error: ": "Slot is already booked"})
+
+    user_id = get_user_id_for_booking(course_code, user_id, username)
+    if user_id is None:
+        return util.send_response(util.HTTP_404_NOT_FOUND, {"error: ": "User not found for booking"})
+
+    if user_has_existing_reservations(user_id, booking_list_id):
+        return util.send_response(util.HTTP_403_FORBIDDEN, {"error: ": "User has existing reservations"})
+
+    reserved_slot = reserve_slot(user_id, booking_list_id, slot_id)
+    if reserved_slot is not None:
+        return util.send_response(util.HTTP_201_CREATED, serialized_reservation(reserved_slot))
+
+    else:
+        return util.send_response(util.HTTP_400_BAD_REQUEST, {"error: ": "Reservation could not be added"})
+
+
+def get_requested_slot(booking_list_id, slot_id):
+    return generate_available_slots(booking_list_id)[int(slot_id)]
+
+
+def slot_is_available(slot):
+    return slot.user_id is None
+
+
+def get_user_id_for_booking(course_code, user_id, username):
+    if username:
+        requested_user_is_privileged = util.get_user_privileges(course_code, user_id)
+        if requested_user_is_privileged:
+            valid_user = ur.get_user_by_username(course_code, username)
+            return valid_user.id if valid_user else None
+    return user_id
+
+
+def serialized_available_slots(booking_list_id):
+    available_slots = generate_available_slots(booking_list_id)
     json_available_slots = [{
         "list_id": available_slot.list_id,
         "sequence_id": available_slot.sequence_id,
@@ -44,7 +82,7 @@ def generate_json_ready_available_slots(booking_list):
     return json_available_slots
 
 
-def generate_json_ready_reservation(reservation):
+def serialized_reservation(reservation):
     return {
         "id": reservation.id,
         "list_id": reservation.list_id,
@@ -54,14 +92,9 @@ def generate_json_ready_reservation(reservation):
 
 
 def reserve_slot(user_id, booking_list_id, sequence_id):
-    new_reservation = Reservation(
-        list_id=booking_list_id,
-        user_id=user_id,
-        sequence_id=sequence_id
-    )
-
+    new_reservation = Reservation(list_id=booking_list_id, user_id=user_id, sequence_id=sequence_id)
     try:
-        insert_reservation(new_reservation)
+        rr.insert_reservation(new_reservation)
     except Exception as exception:
         print(f'Error: {str(exception)}')
         return None
@@ -69,58 +102,51 @@ def reserve_slot(user_id, booking_list_id, sequence_id):
 
 
 def generate_user_reservation_entries(course_code, user_id):
-    user_reservations = retrieve_user_reservations(course_code, user_id)
-
+    user_reservations = rr.retrieve_user_reservations(course_code, user_id)
     if user_reservations is None:
         return None
-
-    user_reservation_entries = []
-
-    for reservation in user_reservations:
-        booking_list = retrieve_booking_list(reservation.list_id)
-        reservation_start_time = booking_list.time + timedelta(minutes=booking_list.interval * reservation.sequence_id)
-
-        user_reservation_entries.append({
-            "id": reservation.id,
-            "start_time": format_datetime(reservation_start_time),
-            "description": booking_list.description,
-            "location": booking_list.location,
-            "length": booking_list.interval
-        })
-
-    return user_reservation_entries
+    return [create_reservation_entry(reservation) for reservation in user_reservations]
 
 
-def format_datetime(datetime_to_format):
-    return datetime_to_format.strftime("%Y-%m-%d %H:%M")
+def create_reservation_entry(reservation):
+    booking_list = blr.retrieve_booking_list(reservation.list_id)
+    start_time = calculate_start_time(booking_list, reservation.sequence_id)
+
+    return {
+        "id": reservation.id,
+        "start_time": process_datetime(start_time),
+        "description": booking_list.description,
+        "location": booking_list.location,
+        "length": booking_list.interval
+    }
+
+
+def calculate_start_time(booking_list, sequence_id):
+    return booking_list.time + timedelta(minutes=booking_list.interval * sequence_id)
+
+
+def process_datetime(datetime_to_process, interval=0, sequence_id=0):
+    datetime_to_process += timedelta(minutes=interval * sequence_id)
+    return datetime_to_process.strftime("%Y-%m-%d %H:%M")
 
 
 def remove_reservation(reservation_id):
     try:
-        delete_reservation(reservation_id)
+        rr.delete_reservation(reservation_id)
     except Exception as exception:
         print(f'Error: {str(exception)}')
-        return False
-    return True
+        return util.HTTP_404_NOT_FOUND, {"error": "Reservation could not be removed."}
+    return util.HTTP_204_NO_CONTENT, None
 
 
 def remove_slot_reservation(booking_list_id, slot_sequence_id):
     try:
-        delete_slot_reservation(booking_list_id, slot_sequence_id)
+        rr.delete_slot_reservation(booking_list_id, slot_sequence_id)
     except Exception as exception:
         print(f'Error: {str(exception)}')
-        return False
-    return True
+        return util.HTTP_404_NOT_FOUND, {"error": "Reservation could not be removed."}
+    return util.HTTP_204_NO_CONTENT, None
 
 
-def get_user_reservations_for_booking_list(user_id, booking_list_id):
-    return retrieve_user_reservations_by_booking_list_id(user_id, booking_list_id)
-
-
-class AvailableSlotDTO:
-    def __init__(self, booking_list_id, sequence_id, start_time):
-        self.list_id = booking_list_id
-        self.sequence_id = sequence_id
-        self.start_time = start_time
-        self.user_id = None
-        self.username = None
+def user_has_existing_reservations(user_id, booking_list_id):
+    return rr.retrieve_user_reservations_by_booking_list_id(user_id, booking_list_id)
