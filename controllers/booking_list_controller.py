e34fec008de0425bf35c0db34b10af8a13d4c210
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-16 14:53:18 +0100

    Refactored: moved most logic and data preparation from controller to service module.

--- controllers/booking_list_controller.py
+++ controllers/booking_list_controller.py
@@ -1,86 +1,46 @@
-from flask import render_template, request, jsonify, make_response, session
-from sqlalchemy.exc import IntegrityError
+from flask import render_template, request, session
 
-from controllers.controller_util import check_privileges, validate_course_code, validate_user_login
-from services.auth_service import get_user_privileges
-from services.booking_list_service import get_booking_lists, add_booking_list, is_invalid_booking_list, \
-    generate_json_ready_booking_lists, remove_booking_list, generate_json_ready_booking_list
-
-
-@validate_course_code
-@validate_user_login
-def manage_booking_lists(course_code):
-    if request.method == "GET":
-        return show_lists_page(course_code, session.get("user_id"))
-
-    if request.method == "POST":
-        return add_new_list(course_code, session.get("user_id"))
-
-
-@validate_course_code
-@validate_user_login
-def delete_booking_list(course_code, booking_list_id):
-    check_privileges(course_code, session.get("user_id"))
-    try:
-        successful_removal = remove_booking_list(course_code, booking_list_id)
-
-        if successful_removal:
-            return make_response(jsonify({}), 204)
-        else:
-            return send_error_response(400)
-
-    except IntegrityError:
-        return send_error_response(422)
-
-
-def show_lists_page(course_code, user_id):
-    booking_lists = get_booking_lists(course_code)
-    user_privileges = get_user_privileges(course_code, user_id)
-
-    json_request = request.headers.get('Accept') == 'application/json'
-    if json_request:
-        return send_success_response(course_code, 200)
-
-    return render_template("booking_lists.html",
-                           course_code=course_code, booking_lists=booking_lists, user_privileges=user_privileges)
-
-
-def add_new_list(course_code, user_id):
-    check_privileges(course_code, user_id)
-
-    json_data = request.get_json()
-    booking_list_dto = BookingListDTO(json_data, course_code)
-
-    if is_invalid_booking_list(booking_list_dto):
-        error_message = jsonify({"error": "Invalid booking data"})
-        return make_response(error_message, 400)
-
-    added_booking_list = add_booking_list(course_code, booking_list_dto)
-
-    if added_booking_list:
-        json_ready_booking_list = generate_json_ready_booking_list(added_booking_list)
-        response_data = {"newBookingList": json_ready_booking_list}
-        return make_response(jsonify(response_data), 201)
-    else:
-        return send_error_response(400)
-
-
-def send_success_response(course_code, status_code):
-    updated_booking_lists = generate_json_ready_booking_lists(course_code)
-    response_data = {"booking_lists": updated_booking_lists}
-    return make_response(jsonify(response_data), status_code)
-
-
-def send_error_response(status_code):
-    error_message = {"error": "Invalid booking data"}
-    return make_response(jsonify(error_message), status_code)
+from services import booking_list_service as bls
+from util import utility as util
 
 
 class BookingListDTO:
-    def __init__(self, json_data, course_code):
+    def __init__(self, json_data, course_code, user_id):
         self.course_id = course_code
+        self.user_id = user_id
         self.description = json_data["description"]
         self.location = json_data.get("location")
         self.time = json_data.get("time")
         self.interval = json_data.get("length")
         self.max_slots = json_data.get("slots")
+
+
+@util.validate_course_code
+@util.validate_user_login
+def manage_booking_lists(course_code):
+    if request.method == "GET":
+        if util.json_request():
+            return util.send_response(util.HTTP_200_OK, {"booking_lists": bls.get_booking_lists(course_code)})
+
+        return render_template("booking_lists.html",
+                               course_code=course_code,
+                               booking_lists=bls.get_booking_lists(course_code),
+                               user_privileges=util.get_user_privileges(course_code, session.get("user_id")))
+
+    if request.method == "POST":
+        return add_new_list(course_code=course_code, user_id=session.get("user_id"))
+
+
+@util.validate_course_code
+@util.validate_user_login
+@util.validate_privileges
+def remove_booking_list(course_code, booking_list_id):
+    return util.send_response(*bls.erase_booking_list(course_code, booking_list_id))
+
+
+@util.validate_privileges
+def add_new_list(course_code, user_id):
+    json_data = request.get_json()
+    booking_list_dto = BookingListDTO(json_data, course_code, user_id)
+    status_code, response_data = bls.add_booking_list(course_code, booking_list_dto)
+    return util.send_response(status_code, response_data)
--- services/booking_list_service.py
+++ services/booking_list_service.py
@@ -1,54 +1,42 @@
 import copy
 
+from sqlalchemy.exc import IntegrityError
+
 from models.booking_list import BookingList
-from repositories.booking_list_repository import retrieve_booking_lists, insert_booking_list, delete_booking_list, \
-    retrieve_booking_list
-from repositories.reservation_repository import retrieve_reservations
+from repositories import booking_list_repository as blr
+from repositories import reservation_repository as rr
+from util import utility as util
 
 
-def get_booking_lists(course_code):
-    booking_lists = retrieve_booking_lists(course_code)
+def get_booking_lists(course_code=None, booking_list_id=None):
+    if course_code:
+        booking_lists = blr.retrieve_booking_lists(course_code)
+        if booking_lists is not None:
+            return processed_booking_lists(booking_lists)
 
-    if booking_lists is None:
-        return []
+    elif booking_list_id:
+        booking_list = blr.retrieve_booking_list(booking_list_id)
+        if booking_list is not None:
+            return processed_booking_lists(booking_list)[0]
 
-    processed_lists = process_time_and_slots(booking_lists)
-    return processed_lists
-
-
-def get_booking_list(booking_id):
-    booking_list = retrieve_booking_list(booking_id)
-
-    if booking_list is None:
-        return []
-
-    processed_list = process_time_and_slots([booking_list])[0]
-
-    return processed_list
+    return []
 
 
-def process_time_and_slots(booking_lists):
-    booking_lists_without_seconds = []
-    for booking_list in booking_lists:
-        list_copy = copy.deepcopy(booking_list)
-        remove_seconds_from_booking_list(list_copy)
-        calculate_available_slots(list_copy)
-        booking_lists_without_seconds.append(list_copy)
-    return booking_lists_without_seconds
-
-
-def remove_seconds_from_booking_list(booking_list):
-    booking_list.time = booking_list.time.strftime('%Y-%m-%d %H:%M')
-
-
-def calculate_available_slots(booking_list):
-    reservations = retrieve_reservations(booking_list.id)
-    booking_list.available_slots = booking_list.max_slots - len(reservations)
+def erase_booking_list(course_code, booking_id):
+    try:
+        blr.delete_booking_list(course_code, booking_id)
+    except IntegrityError:
+        return util.HTTP_422_UNPROCESSABLE_ENTITY, {"error": "Booking list is in use"}
+    return util.HTTP_204_NO_CONTENT, None
 
 
 def add_booking_list(course_code, booking_list_dto):
+    if invalid_booking_list(booking_list_dto):
+        return util.HTTP_400_BAD_REQUEST, {"error": "Invalid booking data"}
+
     new_booking_list = BookingList(
         course_id=course_code.upper(),
+        user_id=booking_list_dto.user_id,
         description=booking_list_dto.description,
         location=booking_list_dto.location,
         time=booking_list_dto.time,
@@ -57,14 +45,49 @@ def add_booking_list(course_code, booking_list_dto):
     )
 
     try:
-        insert_booking_list(new_booking_list)
+        blr.insert_booking_list(new_booking_list)
     except Exception as exception:
         print(f'Error: {str(exception)}')
-        return None
-    return new_booking_list
+        return util.HTTP_422_UNPROCESSABLE_ENTITY, {"error": "Could not add booking list"}
+
+    return util.HTTP_201_CREATED, {'newBookingList': serialized_booking_list(new_booking_list)}
 
 
-def is_invalid_booking_list(booking_list_dto):
+def generate_processed_booking_list(booking_list_id):
+    booking_list = [blr.retrieve_booking_list(booking_list_id)]
+    if booking_list is not None:
+        return processed_booking_lists(booking_list)[0]
+    return None
+
+
+def processed_booking_lists(booking_lists):
+    booking_lists_without_seconds = []
+    for booking_list in booking_lists:
+        list_copy = copy.deepcopy(booking_list)
+        list_copy.time = booking_list.time
+        list_copy.max_slots = calculate_available_slots(list_copy)
+        booking_lists_without_seconds.append(serialized_booking_list(list_copy))
+    return booking_lists_without_seconds
+
+
+def calculate_available_slots(booking_list):
+    reservations = rr.retrieve_reservations(booking_list.id)
+    return booking_list.max_slots - len(reservations)
+
+
+def serialized_booking_list(booking_list):
+    json_booking_list = {
+        "id": booking_list.id,
+        "description": booking_list.description,
+        "location": booking_list.location,
+        "time": booking_list.time.strftime('%Y-%m-%d %H:%M'),
+        "interval": booking_list.interval,
+        "available_slots": booking_list.max_slots
+    }
+    return json_booking_list
+
+
+def invalid_booking_list(booking_list_dto):
     return any(value is None for value in (
         booking_list_dto.description,
         booking_list_dto.location,
@@ -72,37 +95,3 @@ def is_invalid_booking_list(booking_list_dto):
         booking_list_dto.interval,
         booking_list_dto.max_slots
     ))
-
-
-def generate_json_ready_booking_lists(course_code):
-    booking_lists = get_booking_lists(course_code)
-    json_booking_lists = [{
-        "id": booking_list.id,
-        "description": booking_list.description,
-        "location": booking_list.location,
-        "time": booking_list.time,
-        "interval": booking_list.interval,
-        "available_slots": booking_list.available_slots
-    } for booking_list in booking_lists]
-
-    return json_booking_lists
-
-
-def generate_json_ready_booking_list(booking_list):
-    json_booking_list = {
-        "id": booking_list.id,
-        "description": booking_list.description,
-        "location": booking_list.location,
-        "time": booking_list.time,
-        "interval": booking_list.interval,
-        "available_slots": booking_list.max_slots
-    }
-    return json_booking_list
-
-
-def remove_booking_list(course_code, booking_id):
-    try:
-        delete_booking_list(course_code, booking_id)
-    except Exception:
-        raise
-    return True
