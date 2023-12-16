af947834de1d417041053419e6872c15020489f9
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-16 14:52:24 +0100

    Modified to also contain user_id.

--- models/booking_list.py
+++ models/booking_list.py
@@ -9,11 +9,13 @@ class BookingList(Base):
 
     id = Column(INTEGER, primary_key=True, autoincrement=True)
     course_id = Column(VARCHAR, ForeignKey('courses.id'), nullable=False)
+    user_id = Column(INTEGER, ForeignKey('users.id'), nullable=False)
     description = Column(VARCHAR, nullable=False)
     location = Column(VARCHAR, nullable=False)
     time = Column(TIMESTAMP, nullable=False)
     interval = Column(INTEGER, nullable=False)
     max_slots = Column(INTEGER, nullable=False)
 
-    course = relationship('Course', back_populates='booking_lists')
-    reservations = relationship('Reservation', back_populates='booking_list')
+    courses = relationship('Course', back_populates='booking_lists')
+    users = relationship('User', back_populates='booking_lists')
+    reservations = relationship('Reservation', back_populates='booking_lists')
