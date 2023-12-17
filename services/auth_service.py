bdb0b2168f7ae9f1651434b22cdcf5410175bdc9
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-17 14:44:20 +0100

    Refactored: decoupled add_new_users and enroll_users and made generate_responses more concise.

--- services/auth_service.py
+++ services/auth_service.py
@@ -7,44 +7,47 @@ from util import utility as util
 
 
 def authenticate(course_code, username, password):
-    user = ur.get_user_by_username(course_code, username)
+    user = ur.get_user_by_username_and_course_code(course_code, username)
     if user is not None and check_password_hash(user.password, password):
         return user
     return None
 
 
 def insert_new_users_and_enrollments(course_code, user_addition_dto):
-    added_users, not_added_users = add_new_users(user_addition_dto.usernames, user_addition_dto.hashed_password)
-    enrolled_users, not_enrolled_users = enroll_users(course_code, added_users, user_addition_dto.elevated_privileges)
+    usernames = [username for username in user_addition_dto.usernames]
+    added_users, not_added_users = add_new_users(usernames, user_addition_dto.hashed_password)
+    enrolled_users, not_enrolled_users = enroll_users(course_code, usernames, user_addition_dto.elevated_privileges)
     return generate_responses(added_users, enrolled_users, not_added_users, not_enrolled_users)
 
 
 def add_new_users(usernames, password):
-    new_users = [User(username=username, password=password) for username in usernames]
+    new_users = []
     failed_entries = []
 
-    for user in new_users:
+    for username in usernames:
         try:
-            ur.insert_users(user)
+            ur.insert_users(User(username=username, password=password))
+            new_users.append(username)
         except Exception as exception:
             print(f'Error: {str(exception)}')
-            failed_entries.append(user.username)
+            failed_entries.append(username)
 
     return (new_users, None) if len(failed_entries) == 0 else (new_users, failed_entries)
 
 
-def enroll_users(course_code, users, privileges):
+def enroll_users(course_code, usernames, privileges):
     new_enrollments = []
     failed_entries = []
 
-    for user in users:
+    for username in usernames:
         try:
+            user = ur.get_user_by_username(username)
             entry = UserCourseRegistration(user_id=user.id, course_id=course_code.upper(), admin=bool(privileges))
             rr.insert_enrollment_entry(entry)
-            new_enrollments.append(user)
+            new_enrollments.append(username)
         except Exception as exception:
             print(f'Error: {str(exception)}')
-            failed_entries.append(user.username)
+            failed_entries.append(username)
 
     return (new_enrollments, None) if len(failed_entries) == 0 else (new_enrollments, failed_entries)
 
@@ -52,21 +55,21 @@ def enroll_users(course_code, users, privileges):
 def generate_responses(added_users, enrolled_users, failed_user_entries, failed_enroll_entries):
     responses = []
 
+    def create_response(status_code, data_key, data_values):
+        responses.append(
+            {'status': status_code, 'data': {data_key: [{'username': username} for username in data_values]}})
+
     if added_users:
-        responses.append(
-            {'status': util.HTTP_201_CREATED, 'data': [{'username': user.username} for user in added_users]})
+        create_response(util.HTTP_201_CREATED, 'addedUsers', added_users)
     if enrolled_users:
-        responses.append(
-            {'status': util.HTTP_201_CREATED, 'data': [{'username': user.username} for user in enrolled_users]})
+        create_response(util.HTTP_201_CREATED, 'enrolledUsers', enrolled_users)
     if failed_user_entries:
-        responses.append(
-            {'status': util.HTTP_409_CONFLICT, 'data': [{'username': username} for username in failed_user_entries]})
+        create_response(util.HTTP_409_CONFLICT, 'failedUserEntries', failed_user_entries)
     if failed_enroll_entries:
-        responses.append(
-            {'status': util.HTTP_409_CONFLICT, 'data': [{'username': username} for username in failed_enroll_entries]})
+        create_response(util.HTTP_409_CONFLICT, 'failedEnrollEntries', failed_enroll_entries)
 
     return responses
 
 
 def retrieve_user_by_username(course_code, username):
-    return ur.get_user_by_username(course_code, username)
+    return ur.get_user_by_username_and_course_code(course_code, username)
