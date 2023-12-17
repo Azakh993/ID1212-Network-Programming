5b909985c9a91ccf2c9670e23f37c3915bf0c625
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-17 14:46:11 +0100

    Added functionality for retrieving any user that is exists, regardless of if student is enrolled in course.

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -4,7 +4,11 @@ from repositories import session
 from repositories.repository_util import get_first_from_database, add_to_database
 
 
-def get_user_by_username(course_code, username):
+def get_user_by_username(username):
+    return get_first_from_database(session.query(User).filter(User.username == username))
+
+
+def get_user_by_username_and_course_code(course_code, username):
     return get_first_from_database(((session.query(User)
                                      .join(UserCourseRegistration))
                                     .filter(UserCourseRegistration.course_id == course_code.upper(),
